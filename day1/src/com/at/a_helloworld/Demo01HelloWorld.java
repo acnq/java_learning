e268a178cd0430c6c68155664d6fe5b6607fa25c
acnq <fanyuanhaotc@163.com>
2026-03-24 22:40:46 +0800

    init commit

---
+++ day1/src/com/at/a_helloworld/Demo01HelloWorld.java
@@ -0,0 +1,11 @@
+package com.at.a_helloworld;
+
+import java.util.Scanner;
+
+public class Demo01HelloWorld {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i = 0;
+        System.out.println(i);
+    }
+}
---
+++ day2/src/com/at/a_scanner/Demo01Scanner.java
@@ -0,0 +1,4 @@
+package com.at.a_scanner;
+
+public class Demo01Scanner {
+}
