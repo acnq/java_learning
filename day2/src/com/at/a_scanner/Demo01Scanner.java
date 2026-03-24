bb22bf98eda1740aff7ac8d7b82636267de1b306
acnq <fanyuanhaotc@163.com>
2026-03-25 00:25:17 +0800

    scanner

--- day2/src/com/at/a_scanner/Demo01Scanner.java
+++ day2/src/com/at/a_scanner/Demo01Scanner.java
@@ -1,4 +1,17 @@
 package com.at.a_scanner;
 
+import java.util.Scanner;
+
 public class Demo01Scanner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int data1 = scanner.nextInt();
+        int data2 = scanner.nextInt();
+
+        int res = data1 * data2;
+        System.out.println(data1 +  "*" + data2 + "=" + res);
+    }
+
+
 }
